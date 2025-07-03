4d9ea91768fa8a10d7bd3e07cf5f83428ce7ac8b
kaidoverse <kaidoverse@gmail.com>
2025-07-03 16:37:44 +0100

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import UserLayout from './component/layout/UserLayout'
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<UserLayout />}>{/*user layout*/}</Route>
+          <Route>{/*admin layout*/}</Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/component/layout/UserLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserLayout = () => {
+    return (
+        <>
+            {/*header */}
+            {/*main content */}
+            {/*footer */}
+        </>
+    )
+}
+
+export default UserLayout
