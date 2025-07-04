1147ddc3464fa7204797c2b3cd763d3ced485f0c
kaidoverse <kaidoverse@gmail.com>
2025-07-04 12:08:06 +0100

    chore: fixed Tailwind config issue and applied consistent styles to Topbar component

---
+++ frontend/src/component/common/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Topbar from '../layout/Topbar'
+
+const Header = () => {
+    return (
+        <div>
+            {/* topbar*/}
+            <Topbar />
+            {/* navbar*/}
+            {/* cart drawer*/}
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/component/layout/Topbar.jsx
@@ -0,0 +1,41 @@
+import { TbBrandMeta } from 'react-icons/tb'
+import { IoLogoInstagram } from 'react-icons/io'
+import { RiTwitterXLine } from 'react-icons/ri'
+import React from 'react'
+
+const Topbar = () => {
+    return (
+        <div className="bg-rabbit-red text-white">
+            <div className='container mx-auto flex  justify-between items-center py-2 px-2'>
+
+                <div className="hidden md:flex items-center space-x-4">
+                    <a href="#" className="hover:text-gray-300">
+                        <TbBrandMeta className='h-5 w-5' />
+                    </a>
+
+                    <a href="#" className="hover:text-gray-300">
+                        <IoLogoInstagram className='h-5 w-5' />
+                    </a>
+
+                    <a href="#" className="hover:text-gray-300">
+                        <RiTwitterXLine className='h-5 w-4' />
+                    </a>
+                </div>
+
+                <div className="text-sm text-center flex-grow">
+                    <span>
+                        we ship worldwide - fast and reliable shipping!
+                    </span>
+                </div>
+
+                <div className="text-sm hidden md:block text-white" >
+                    <a href="tel:233543608711" className="hover:text-gray-300">+233 (543) 608-711</a>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Topbar
