28b0c0822ca446d524fadd4c8de3e05a8f8ed0d8
kaidoverse <kaidoverse@gmail.com>
2025-07-05 13:51:23 +0100

    add cart product layout with quantity controls and delete icon

--- frontend/src/component/layout/CartDrawer.jsx
+++ frontend/src/component/layout/CartDrawer.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { IoMdClose } from 'react-icons/io';
+import CartContents from '../cart/CartContents';
 
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
 
 
     return (
         <div
-            className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
+            className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
 
             {/*close button*/}
             <div className='flex justify-end p-4'>
@@ -17,7 +18,7 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
             {/* Cart content scrollable area */}
             <div className="flex-grow p-4 overflow-y-auto">
                 <h2 className='text-xl font-semibold mb-4'>your cart</h2>
-                {/* component for cart content */}
+                <CartContents />
             </div>
             {/* checkout button */}
             <div className="p-4 bg-white sticky bottom-0">
